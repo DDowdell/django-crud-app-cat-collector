8a373724b9d0e636364e15c688715dc11fc0cd3f
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-10 11:22:39 -0400

    create new

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-  path('cats/', views.cat_index, name='cat-index'),
-  path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("cats/", views.cat_index, name="cat-index"),
+    path("cats/<int:cat_id>/", views.cat_detail, name="cat-detail"),
+    path("cats/create/", views.CatCreate.as_view(), name="cat-create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Cat
+
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def cat_index(request):
     cats = Cat.objects.all()  # look familiar?
-    return render(request, 'cats/index.html', {'cats': cats})
+    return render(request, "cats/index.html", {"cats": cats})
+
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    return render(request, "cats/detail.html", {"cat": cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
