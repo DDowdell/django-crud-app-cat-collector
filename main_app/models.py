8d020f29fd89ecd28b2bbed215168e8333af159b
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-10 12:20:35 -0400

    added feedings

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples added above our models
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +19,20 @@ class Cat(models.Model):
     
     def get_absolute_url(self):
         return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+     # Create a cat_id column for each feeding in the database
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+
