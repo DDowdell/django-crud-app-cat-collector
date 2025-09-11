10dfb598173929b1207ff920589f9c0dac9021ae
Dae Dowdell <dow_dell2010@yahoo.com>
2025-09-11 11:33:57 -0400

    toy crud

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.views.generic import ListView, DetailView # add these 
+from .models import Cat, Toy
 from .forms import FeedingForm
 
 
@@ -60,3 +61,22 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+    
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+    
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+    
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
